b19194fd063d8390d7907e88cb86a5ca739f47b6
MarcoServinDLT <marco.servin@alumnos.udg.mx>
2024-12-02 17:25:38 -0600

    Formato de código.

--- challenges/Day2/challenge-2.ts
+++ challenges/Day2/challenge-2.ts
@@ -2,7 +2,8 @@ export function createFrame(names: string[]): string {
     const maxLenght = Math.max( ...names.map( (n) => n.length ) );
     const top = ''.padEnd(maxLenght+4, '*');
 
-    return [ top,
+    return [ 
+        top,
         ...names.map( (name: string) => `* ${name.padEnd(maxLenght)} *` ),
         top
     ].join("\n")
