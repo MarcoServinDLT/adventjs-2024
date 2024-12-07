eed3e3d267d94ca014b1c689260d89ac3cc195a4
MarcoServinDLT <marco.servin@alumnos.udg.mx>
2024-12-06 20:25:46 -0600

    Mejoras a la solución del día 5.

--- challenges/Day5/challenge-5.ts
+++ challenges/Day5/challenge-5.ts
@@ -5,12 +5,14 @@ export type Shoe = {
 
 export function organizeShoes(shoes: any): number[] {
     const boots = {};
-    shoes.forEach((shoe) => {
-        boots[shoe.size] ??= {};
-        boots[shoe.size][shoe.type] ??= 0;
-        boots[shoe.size][shoe.type]++
-    });
-    return Object.keys(boots).reduce((pairs: any, size: any) => 
-       [...pairs, ...new Array( Math.min(boots[size]['I'] ?? 0, boots[size]['R'] ?? 0) ).fill(+size) ]
-    , []);
+    const result = [];
+    for(const shoe of shoes) {
+        boots[shoe.size] ??= { 'I':0, 'R': 0};
+        boots[shoe.size][shoe.type]++;
+    }
+    for(const [size, types] of Object.entries(boots)) {
+        const pair = Math.min(types['I'], types['R']);
+        result.push(...Array(pair).fill(+size));
+    }
+    return result;
 }
